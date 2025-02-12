44b4cc75f667508bf5b544ee3d4d821c05edf914
sadal <sadalamartin@gmail.com>
2025-02-12 13:23:22 +0300

    Bima Project

---
+++ src/main/java/TZ/TPDF/Bima/BimaApplication.java
@@ -0,0 +1,13 @@
+package TZ.TPDF.Bima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TZ/TPDF/Bima/BimaApplicationTests.java
@@ -0,0 +1,13 @@
+package TZ.TPDF.Bima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
